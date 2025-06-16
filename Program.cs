2f6000df46e0b573b500c636997ed5b257643b82
Deepak3015 <D03748651@gmail.com>
2025-06-16 14:32:48 +0530

    Same repo with different project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+class Login
+
+{
+    static void Main()
+    {
+        Console.WriteLine("===Simple Creditionals===");
+
+        //Assign creditionacial
+        string correct_username = "Admin123";
+        string correct_password = "Admin@123";
+
+        //Taking user_input 
+
+        Console.Write("Enter your User name :");
+        string user_username = Console.ReadLine();
+
+        Console.Write("Enter your password :");
+        string user_password = Console.ReadLine();
+
+        //Autorization woth if else statement
+
+        if (correct_username == user_username && correct_password == user_password)
+        {
+            Console.WriteLine("Login Succesfull");
+        }
+        else
+        {
+            Console.WriteLine("Wrong usernam II password");
+
+        }
+
+    }
+}
