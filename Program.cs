dc2a018030764bd1d2288ebb29d8ba530e5af620
Deepak3015 <D03748651@gmail.com>
2025-06-16 13:36:54 +0530

    again firts commit of the day

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("=== Simple Calculator ===");
+
+//         Console.Write("Enter your First Number: ");
+//         string input1 = Console.ReadLine();
+//         double num1 = Convert.ToDouble(input1);
+
+//         Console.WriteLine("You Entered: " + num1);
+//     }
+// }
+// //
+
+
+// using System;
+
+// class Calculator {
+
+//     static void Main()
+//     {
+//         Console.Write("Enter your First Number :");
+//         Double num1 = Convert.ToDouble(Console.ReadLine());
+
+//         Console.Write("Choose your Operator (+,-,*,/):");
+//         string op = Console.ReadLine();
+
+//         Console.Write("Enter your Second Number:-");
+//         Double num2 = Convert.ToDouble(Console.ReadLine());
+
+//         Double Result = 0;
+
+//         if (op == "+")
+//             Result = num1 + num2;
+//         else if (op == "-")
+//             Result = num1 - num2;
+//         else if (op == "*")
+//             Result = num1 * num2;
+//         else if (op == "/")
+//             Result = num1 / num2;
+//         Console.WriteLine("Result:" + Result);
+//     }
+
+// }
